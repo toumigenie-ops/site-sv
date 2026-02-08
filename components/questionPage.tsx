7a0624d45b4b3e0cd22d7c6facd7658d08a2208a
toumigenie-ops <genie.toumi-kandza@epitech.eu>
2026-02-08 20:44:24 +0100

    Add photos

--- components/questionPage.tsx
+++ components/questionPage.tsx
@@ -47,7 +47,7 @@ const QuestionPage = () => {
         <div className="flex-1 flex flex-col">
           <div style={{ filter: 'grayscale(80%)' }} className="h-80 rounded-3xl overflow-hidden shadow-2xl mb-6">
             <ImageWithFallback
-              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=500&q=80"
+              src="components/assets/IMG_5162.jpeg"
               alt="Personne seule"
               width={500}
               height={400}
